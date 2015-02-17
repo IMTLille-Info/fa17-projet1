137c02389b37337e48d14f3f7d5f22739a5738c7
Calooe <vincen59@hotmail.fr>
2015-02-17 11:21:00 +0100

    Creation du main

---
+++ bomberman/src/uvinfo/bomberman/BombermanGame.java
@@ -0,0 +1,14 @@
+package uvinfo.bomberman;
+
+public class BombermanGame {
+
+	public BombermanGame() {
+		
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Ok t'es dans le main...");
+	}
+
+}
