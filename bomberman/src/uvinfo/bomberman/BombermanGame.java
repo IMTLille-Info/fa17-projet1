25e458d020798990bfeaae9ab1d7ff5364137997
Calooe <vincen59@hotmail.fr>
2015-02-17 11:53:00 +0100

    Affichage d'une fenetre de jeu
    //il faut ajouter les librairies Slick2D et surtout ajouter lwjgl.dll
    dans le dossier du projet (à coté du .project)

--- bomberman/src/uvinfo/bomberman/BombermanGame.java
+++ bomberman/src/uvinfo/bomberman/BombermanGame.java
@@ -1,14 +1,48 @@
 package uvinfo.bomberman;
 
-public class BombermanGame {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	public BombermanGame() {
-		
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+
+public class BombermanGame extends BasicGame {
+
+	public BombermanGame(String gamename) {
+		super(gamename);
 	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.drawString("Howdy!", 50, 50);
+	}	
+	
+	
 
 	public static void main(String[] args) {
 
-		System.out.println("Ok t'es dans le main...");
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new BombermanGame("Projet 1 : Bomberman"));
+			appgc.setDisplayMode(640, 480, true);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(BombermanGame.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 
 }
