787ee558cba9eff676a13bfb3f7e38cead4d26f2
cedricmorin1 <Cedric@MacBook-Pro-de-Cedric-2.local>
2015-02-17 14:58:34 +0100

    Début du projet arborescence + librairie

---
+++ bomberman/src/uvinfo/bomberman/WindowGame.java
@@ -0,0 +1,63 @@
+package uvinfo.bomberman;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+
+public class WindowGame extends BasicGame {
+	private GameContainer container;
+
+
+	public WindowGame() {
+		super("Lesson 1 :: window Game");
+	}
+
+	@Override
+	/** GameContainer methode permettant d'initialiser le contenu du 
+	 * du jeu , charger les graphismes, la musique etc.. 
+	 */
+	public void init(GameContainer container) throws SlickException {
+		
+		this.container = container;
+	
+	}
+
+	@Override
+	/**render(GameContainer, Graphics) : doit afficher le 
+	 * contenu du jeux
+	 */
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+	}
+
+	@Override
+	/**update(GameContainer, int) : doit mettre à jour les élément de 
+	 * la scène en fonction du delta temps qui est survenu. C’est ici que la 
+	 * logique du jeux est renfermé.
+	 */
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+	}
+	
+	/** methode appelé à chaque relâchement de touche 
+	 * 
+	 * @param key 
+	 * @param c
+	 */
+	public void KeyRealeased(int key, char c){
+		 if (Input.KEY_ESCAPE == key) {
+	            container.exit();
+	        }
+	}
+	
+	public static void main(String[] args) throws SlickException {
+		/**GameContainer, ce container permet de configurer environnement 
+		 * d’exécution de la boucle.
+		 */
+		new AppGameContainer(new WindowGame(), 640, 480, false).start();
+	}
+
+}
