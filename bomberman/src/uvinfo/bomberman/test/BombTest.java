c43533a982cae907d159570dfb83bddfea071cd4
tonyop62 <tonyop62@live.fr>
2015-02-23 20:57:47 +0100

    Tests unitaires de Bomb (fonctionne pas à cause de Image et SpriteSheet)

--- bomberman/src/uvinfo/bomberman/test/BombTest.java
+++ bomberman/src/uvinfo/bomberman/test/BombTest.java
@@ -4,18 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Image;
 
 import uvinfo.bomberman.Bomb;
 
 public class BombTest {
 
 	@Test
-	public void testSetCoordonnées() throws SlickException {
-		Bomb bomb = new Bomb();
+	public void testSetCoordonnees() throws SlickException {
+		Bomb bomb = new Bomb(8);
 		bomb.setCoordonnees(4, 6);
 		assertEquals(4, bomb.getPosX());
 		assertEquals(6, bomb.getPosY());
-		
+	}
+	
+	@Test
+	public void testSetTimeBeforeExplode() throws SlickException{
+		Bomb bomb = new Bomb(8);
+		bomb.getAnimation().addFrame(new Image("sprites/bomb.png"), 100);
+		bomb.setTimeBeforeExplode(50);
+		assertEquals(50, bomb.getAnimation().getDuration(1));
 	}
 
 }
