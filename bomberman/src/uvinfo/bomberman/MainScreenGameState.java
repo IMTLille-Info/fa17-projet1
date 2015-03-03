2345f6be774d8a45a0c17e94c34b899e6b0ca136
cedricmorin1 <Cedric@192.168.13.121>
2015-03-03 16:50:45 +0100

    Test avec shape

--- bomberman/src/uvinfo/bomberman/MainScreenGameState.java
+++ bomberman/src/uvinfo/bomberman/MainScreenGameState.java
@@ -5,11 +5,17 @@ import javax.swing.JComboBox;
 
  
 
+
+
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -19,17 +25,20 @@ public class MainScreenGameState extends BasicGameState {
 	public static final int ID = 1;
 	private Image background;
 	private StateBasedGame game;
-
-
+	Shape cercle;
+	Shape rect;
+	
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
 		this.game = game;
 		this.background = new Image("res/accueilDragon.png");
-		String Bombe = JOptionPane.showInputDialog("Combien voulez vous de bombe ? ");
-		String nbBombe = JOptionPane.showInputDialog("Combien voulez vous de bombe  a? ");
-
-	  
+		//String Bombe = JOptionPane.showInputDialog("Combien voulez vous de bombe ? ");
+		//String nbBombe = JOptionPane.showInputDialog("Combien voulez vous de bombe  a? ");
+		rect = new Rectangle(50, 50, 400, 200);
+		cercle = new Circle(70, 70, 32);
+		
+	   
 	}
 
 	/**
