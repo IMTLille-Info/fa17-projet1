0c5a1c2d1cf844f5d5d6888f19ac5a37428ccc94
tonyop62 <tonyop62@live.fr>
2015-03-09 13:26:02 +0100

    Séparation en plusieurs fonctions des tests de la méthode hurt des classes Bomb et SuperBomb

--- bomberman/src/uvinfo/bomberman/test/BombTest.java
+++ bomberman/src/uvinfo/bomberman/test/BombTest.java
@@ -106,7 +106,7 @@ public class BombTest {
 	}
 	
 	@Test
-	public void testHurt() throws InterruptedException, SlickException{
+	public void testHurtAvatar() {
 		// avatar a coté de la bombe
 		Bomb bomb = new Bomb();
 		bomb.pose(335, 305);
@@ -115,56 +115,85 @@ public class BombTest {
 		bomb.hurt(av);
 		
 		assertEquals(8, av.getLife()); // points de vie avatar baissée de 2
+	}
+	
+	@Test
+	public void testHurtAvatarTooFar() {
 		
 		// avatar trop éloigné de la bombe ou hors de l'axe de l'explosion
+		Bomb bomb = new Bomb();
 		bomb.pose(102, 102);
 		bomb.setExploding(true);
+		Avatar av = new Avatar(); 
 		bomb.hurt(av);
 		
-		assertEquals(8, av.getLife()); 	// points de vie du avatar intacts
+		assertEquals(10, av.getLife()); 	// points de vie du avatar intacts
 		
+	}
+		
+	@Test
+	public void testHurtAvatarEverHurted() {
 		// avatar a coté de la bombe mais déjà touché par la bombe
+		Bomb bomb = new Bomb();
 		bomb.pose(335, 305);
 		bomb.setExploding(true);
+		Avatar av = new Avatar();
 		av.setHasBeenHurted(true);
 		bomb.hurt(av);
 		
-		assertEquals(8, av.getLife()); // points de vie du avatar intacts
-		
+		assertEquals(10, av.getLife()); // points de vie du avatar intacts
+	}
+	
+	@Test
+	public void testHurtMonstre(){
 		// monstre a coté de la bombe
+		Bomb bomb = new Bomb();
 		bomb.pose(335, 305); 
 		bomb.setExploding(true);
 		Personnage monstre = new Monstre();
 		bomb.hurt(monstre);
 		
 		assertEquals(8, monstre.getLife()); // points de vie monstre baissée de 2
-		
+	}
+	
+	@Test
+	public void testHurtMonstreTooFar() {
 		// monstre trop éloigné de la bombe ou hors de l'axe de l'explosion
+		Bomb bomb = new Bomb();
 		bomb.pose(102, 102);
 		bomb.setExploding(true);
+		Personnage monstre = new Monstre();
 		bomb.hurt(monstre);
 	
-		assertEquals(8, monstre.getLife()); // points de vie du monstre intacts
+		assertEquals(10, monstre.getLife()); // points de vie du monstre intacts
+	}
 		
-		// monstre a coté de la bombe mais déjà toucgé par la bombe
+	@Test
+	public void testHurtMonstreEverHurted() {
+		// monstre a coté de la bombe mais déjà touché par la bombe
+		Bomb bomb = new Bomb();
 		bomb.pose(335, 305);
 		bomb.setExploding(true);
+		Personnage monstre = new Monstre();
 		monstre.setHasBeenHurted(true);
 		bomb.hurt(monstre);
-		bomb.hurt(av);
 		
-		assertEquals(8, monstre.getLife()); // points de vie du monstre intacts
-		
+		assertEquals(10, monstre.getLife()); // points de vie du monstre intacts
+	}
+	
+	@Test
+	public void testHurtEverybody() {
 		// monstre et avatar touché en même temps
+		Bomb bomb = new Bomb();
 		bomb.pose(335, 305);
 		bomb.setExploding(true);
-		monstre.setHasBeenHurted(false);
-		av.setHasBeenHurted(false);
+		Personnage monstre = new Monstre();
+		Personnage av = new Avatar();
 		bomb.hurt(monstre);
 		bomb.hurt(av);
 		
-		assertEquals(6, av.getLife());
-		assertEquals(6, monstre.getLife());
+		assertEquals(8, av.getLife());
+		assertEquals(8, monstre.getLife());
 	}
 	
 
--- bomberman/src/uvinfo/bomberman/test/SuperBombTest.java
+++ bomberman/src/uvinfo/bomberman/test/SuperBombTest.java
@@ -13,7 +13,7 @@ import uvinfo.bomberman.SuperBomb;
 public class SuperBombTest {
 
 	@Test
-	public void testHurt() {
+	public void testHurtAvatar() {
 		// avatar a coté de la bombe
 		Bomb bomb = new SuperBomb();
 		bomb.pose(210, 305);
@@ -22,55 +22,83 @@ public class SuperBombTest {
 		bomb.hurt(av);
 		
 		assertEquals(6, av.getLife()); // points de vie avatar baissée de 2
-		
+	}
+	
+	@Test
+	public void testHurtAvatarTooFar() {
 		// avatar trop éloigné de la bombe ou hors de l'axe de l'explosion
+		Bomb bomb = new SuperBomb();
 		bomb.pose(102, 102);
 		bomb.setExploding(true);
+		Avatar av = new Avatar();
 		bomb.hurt(av);
 		
-		assertEquals(6, av.getLife()); 	// points de vie du avatar intacts
+		assertEquals(10, av.getLife()); 	// points de vie du avatar intacts
+	}
 		
+	@Test
+	public void testHurtAvatarEverHurted() {
 		// avatar a coté de la bombe mais déjà touché par la bombe
+		Bomb bomb = new SuperBomb();
 		bomb.pose(335, 305);
 		bomb.setExploding(true);
+		Avatar av = new Avatar();
 		av.setHasBeenHurted(true);
 		bomb.hurt(av);
 		
-		assertEquals(6, av.getLife()); // points de vie du avatar intacts
-		
+		assertEquals(10, av.getLife()); // points de vie du avatar intacts
+	}
+	
+	@Test
+	public void testHurtMonstre() {
 		// monstre a coté de la bombe
+		Bomb bomb = new SuperBomb();
 		bomb.pose(550, 305); 
 		bomb.setExploding(true);
 		Personnage monstre = new Monstre();
 		bomb.hurt(monstre);
 		
 		assertEquals(6, monstre.getLife()); // points de vie monstre baissée de 2
+	}
 		
+	@Test
+	public void testHurtMonstreTooFar() {
 		// monstre trop éloigné de la bombe ou hors de l'axe de l'explosion
+		Bomb bomb = new SuperBomb();
 		bomb.pose(102, 102);
 		bomb.setExploding(true);
+		Personnage monstre = new Monstre();
 		bomb.hurt(monstre);
 	
-		assertEquals(6, monstre.getLife()); // points de vie du monstre intacts
+		assertEquals(10, monstre.getLife()); // points de vie du monstre intacts
+	}
 		
-		// monstre a coté de la bombe mais déjà toucgé par la bombe
+	@Test
+	public void testHurtMonstreEverHurted() {
+		// monstre a coté de la bombe mais déjà touché par la bombe
+		Bomb bomb = new SuperBomb();
 		bomb.pose(335, 305);
 		bomb.setExploding(true);
+		Personnage monstre = new Monstre();
 		monstre.setHasBeenHurted(true);
 		bomb.hurt(monstre);
 		
-		assertEquals(6, monstre.getLife()); // points de vie du monstre intacts
+		assertEquals(10, monstre.getLife()); // points de vie du monstre intacts
+	}
 		
+	@Test
+	public void testHurtEverybody() {
 		// monstre et avatar touché en même temps
+		Bomb bomb = new SuperBomb();
 		bomb.pose(335, 305);
 		bomb.setExploding(true);
-		monstre.setHasBeenHurted(false);
-		av.setHasBeenHurted(false);
+		Personnage monstre = new Monstre();
+		Avatar av = new Avatar();
 		bomb.hurt(monstre);
 		bomb.hurt(av);
 		
-		assertEquals(2, av.getLife());
-		assertEquals(2, monstre.getLife());
+		assertEquals(6, av.getLife());
+		assertEquals(6, monstre.getLife());
 	}
 
 }
