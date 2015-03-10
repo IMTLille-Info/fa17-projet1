badb9318b42124a4f953b3db22dfe0cbeb4dc0ce
tonyop62 <tonyop62@live.fr>
2015-03-10 21:01:58 +0100

    classe pour tester kryonet mais marche pas :(

---
+++ bomberman/src/uvinfo/bomberman/testKryon.java
@@ -0,0 +1,47 @@
+package uvinfo.bomberman;
+
+import com.esotericsoftware.kryonet.*;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import javax.swing.DefaultListModel;
+import javax.swing.DefaultListSelectionModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.minlog.Log;
+
+
+public class testKryon {
+	Server server = new Server() {
+		protected Connection newConnection () {
+			// By providing our own connection implementation, we can store per
+			// connection state without a connection ID to state look up.
+			return new ChatConnection();
+		}
+	}
+			
+    
+    
+    client = new Client();
+    client.start();
+
